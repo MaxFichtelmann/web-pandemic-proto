02ee1f12575da41cbeec35a18c182eaca80f19fd
Stefan Holzmüller <stefanholzmueller@github>
2016-08-03 21:02:52 +0200

    moved (most) logic into Elm code

--- elm/Main.elm
+++ elm/Main.elm
@@ -6,9 +6,11 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Debug exposing (..)
 
-port js2elm : (String -> msg) -> Sub msg
+port click : (String -> msg) -> Sub msg
 
-port elm2js : String -> Cmd msg
+port townCreate : (String, Int, Int) -> Cmd msg
+port playerCreate : (String, String, String) -> Cmd msg
+port playerMove : (String, String) -> Cmd msg
 
 main =
   App.program
@@ -18,19 +20,36 @@ main =
     , subscriptions = subscriptions
     }
 
-type alias Model = String
+type alias Model = { towns: List Town
+                   , player: Player
+                   }
+
+type alias Town = { name : String
+                  , x : Int
+                  , y : Int
+                  }
+
+type alias Player = { name : String
+                    , color : String
+                    , townName : String
+                    }
 
 init : (Model, Cmd Msg)
-init =
-  ("", Cmd.none)
+init = ({ towns = [ { name = "leipzig", x = 1, y = 1 }
+                  , { name = "tennenlohe", x = 4, y = 2 }
+                  ]
+        , player = { name = "max", color = "blue", townName = "leipzig" }
+        }, Cmd.batch [ townCreate ("leipzig", 1, 1), townCreate ("tennenlohe", 4, 2), playerCreate ("max", "blue", "leipzig")])
 
-type Msg = MsgFromJS String
+type Msg = Click String
 
 subscriptions : Model -> Sub Msg
-subscriptions model = js2elm MsgFromJS
+subscriptions model = click Click
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update (MsgFromJS str) model = ( log "elm" str, Cmd.none )
+update (Click targetTown) model = let currentPlayer = model.player
+                                      updatedPlayer = { currentPlayer | townName = targetTown }
+                                  in  ( { model | player = updatedPlayer }, playerMove ("max", targetTown))
 
 view : Model -> Html Msg
 view model = div [] []
--- js/index.js
+++ js/index.js
@@ -5,26 +5,17 @@ const canvas = new fabric.Canvas('canvas')
 const factory = require('./lib/factory')
 const elm = Elm.Main.worker()
 
-const fakeElm = {
-  channels: {},
-  subscribe: function(channel, callback) {
-    this.channels[channel] = callback
-  },
-  emit: function(channel, ...data) {
-    this.channels[channel](...data)
-  }
-}
-
 const towns = {}
 const players = {}
 
 canvas.on('mouse:up', (options) => {
   if(options.target) {
-    fakeElm.emit('click', options.target.type, options.target.id)
+    elm.ports.click.send(options.target.id)
   }
 })
 
-fakeElm.subscribe('town:create', (name, x, y) => {
+elm.ports.townCreate.subscribe(function(tuple) {
+  var [name, x, y] = tuple;
   const town = factory.createTown(x, y)
   town.id = name
   town.type = 'town'
@@ -32,7 +23,8 @@ fakeElm.subscribe('town:create', (name, x, y) => {
   towns[name] = town
 })
 
-fakeElm.subscribe('player:create', (name, color, startTown) => {
+elm.ports.playerCreate.subscribe(function(tuple) {
+  var [name, color, startTown] = tuple;
   const player = factory.createPlayer(color, towns[startTown])
   player.id = name
   player.type = 'player'
@@ -40,7 +32,8 @@ fakeElm.subscribe('player:create', (name, color, startTown) => {
   players[name] = player
 })
 
-fakeElm.subscribe('player:move', (playerName, townName) => {
+elm.ports.playerMove.subscribe(function(tuple) {
+  var [playerName, townName] = tuple;
   const town = towns[townName]
   players[playerName].animate({
     'left': town.left + factory.options.gap,
@@ -48,51 +41,23 @@ fakeElm.subscribe('player:move', (playerName, townName) => {
   }, {
     onChange: canvas.renderAll.bind(canvas),
     onComplete: () => {
-      fakeElm.emit('player:moved', playerName, townName)
+//      elm.ports.js2elm.send(["player:moved", playerName, townName])
     }
   })
 })
 
 // logic to be moved to elm below
 
-let currentPlayer = 'max';
-const elmModel = {
-  towns: {
-    leipzig: {
-      x:1, y:1
-    },
-    tennenlohe: {
-      x:4, y:2
-    }
-  },
-  players: {
-    max: {
-      color: 'blue',
-      town: 'leipzig'
-    }
-  }
-}
-
-Object.keys(elmModel.towns).forEach(function(name) {
-  const town = elmModel.towns[name]
-  fakeElm.emit('town:create', name, town.x, town.y)
-});
-
-Object.keys(elmModel.players).forEach(function(name) {
-  const player = elmModel.players[name]
-  fakeElm.emit('player:create', name, player.color, player.town)
-});
-
-fakeElm.subscribe('click', (type, id) => {
-  if (type === 'town' ) {
-    const town = towns[id]
-    if (id !== elmModel.players[currentPlayer].town) {
-      fakeElm.emit('player:move', currentPlayer, id)
-    }
-  }
-})
-
-fakeElm.subscribe('player:moved', (playerName, townName) => {
-  elmModel.players[playerName].town = townName
-  console.log(`moved ${playerName} to ${townName}`)
-})
+// fakeElm.subscribe('click', (type, id) => {
+//   if (type === 'town' ) {
+//     const town = towns[id]
+//     if (id !== elmModel.players[currentPlayer].town) {
+//       fakeElm.emit('player:move', currentPlayer, id)
+//     }
+//   }
+// })
+//
+// fakeElm.subscribe('player:moved', (playerName, townName) => {
+//   elmModel.players[playerName].town = townName
+//   console.log(`moved ${playerName} to ${townName}`)
+// })
